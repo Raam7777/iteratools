50d2950a61f1f38e65e58d1ec4f02181e89dbc11
Raam7777 <Raam7777@gmail.com>
2020-06-09 09:46:50 +0300

    first commit - range work, no compile

---
+++ accumulate.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace itertools {
+  template<typename T> 
+  class accumulate{
+      T acc;
+      
+  public:
+      accumulate(T _acc): acc(_acc){}
+
+
+
+
+
+  };
+}
---
+++ range.hpp
@@ -0,0 +1,57 @@
+#pragma once
+
+namespace itertools {
+
+template <typename T>
+class range
+{
+
+public:
+  T r1;
+  T r2;
+
+  range(const T r1_begin, const T r2_end) : r1(r1_begin), r2(r2_end){}
+
+
+  class iterator
+  {
+  public:
+    T iter;
+
+    iterator(T it) : iter(it) {}
+
+    iterator(const iterator& other) : iter(other.iter) {}
+
+    const T operator*() const
+    {
+      return iter;
+    }
+
+    iterator &operator++()
+    {
+      ++iter;
+      return *this;
+    }
+
+    bool operator!=(range::iterator const &it) const
+    {
+      return iter != it.iter;
+    }
+
+
+
+  };
+
+iterator begin()const
+{
+    return range::iterator(r1);
+}
+
+iterator end()const
+{
+  return range::iterator(r2);
+}
+
+};
+
+}
